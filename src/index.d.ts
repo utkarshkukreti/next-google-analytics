0391b1ded736adc6ccf9947ef8320a2f74e1f958
Utkarsh Kukreti <utkarshkukreti@gmail.com>
2020-11-05 09:59:41 +0530

    also generate .d.ts

---
+++ src/index.d.ts
@@ -0,0 +1,10 @@
+/// <reference types="react" />
+export declare const Head: () => JSX.Element | undefined;
+export declare const pageview: (url: string) => void;
+export declare const event: ({ action, category, label, value, }: {
+    action: string;
+    category?: string | undefined;
+    label?: string | undefined;
+    value?: string | undefined;
+}) => void;
+export declare const useAppInit: () => void;
