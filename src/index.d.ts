c5568fd3108187d46c4252d18420fb6bb61e8f96
Utkarsh Kukreti <utkarshkukreti@gmail.com>
2024-05-07 14:30:33 +0530

    update dependencies

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,12 +1,12 @@
-/// <reference types="react" />
 import { NextWebVitalsMetric } from 'next/app';
-export declare const Head: () => JSX.Element;
+import * as React from 'react';
+export declare const Head: () => React.JSX.Element;
 export declare const pageview: (url: string) => void;
 export declare const event: ({ action, category, label, value, }: {
     action: string;
-    category?: string | undefined;
-    label?: string | undefined;
-    value?: string | undefined;
+    category?: string;
+    label?: string;
+    value?: string;
 }) => void;
 export declare const useAppInit: () => void;
 export declare const reportWebVitals: ({ id, name, label, value, }: NextWebVitalsMetric) => void;
